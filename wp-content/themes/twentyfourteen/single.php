633b28c8bec0830754553616dcd0cbf560cc24d3
robseptian <robseptian@gmail.com>
2017-02-18 20:10:41 +0700

    a

--- wp-content/themes/twentyfourteen/single.php
+++ wp-content/themes/twentyfourteen/single.php
@@ -14,7 +14,7 @@ get_header(); ?>
         <div class="dez-bnr-inr overlay-black-middle" style="background-image:url(<?php echo get_template_directory_uri() ?>/assets/images/background/bg4.jpg);">
             <div class="container">
                 <div class="dez-bnr-inr-entry">
-                    <h1 class="text-white">Blog single on sidebar</h1>
+                    <h1 class="text-white">Blog single with sidebar</h1>
                 </div>
             </div>
         </div>
@@ -24,70 +24,138 @@ get_header(); ?>
             <div class="container">
                 <ul class="list-inline">
                     <li><a href="#">Home</a></li>
-                    <li>Blog single on sidebar</li>
+                    <li>Blog single with sidebar</li>
                 </ul>
             </div>
         </div>
         <!-- Breadcrumb row END -->
         <div class="content-area">
             <div class="container">
-                <!-- blog start -->
-                <div class="blog-post blog-single">
-                    <div class="dez-post-title ">
-                        <h3 class="post-title"><a href="#">Title of first blog post</a></h3>
+                <div class="row">
+                    <!-- Left part start -->
+                    <div class="col-md-9">
+                        <!-- blog start -->
+                        <div class="blog-post blog-single">
+                            <div class="dez-post-title ">
+                                <h3 class="post-title"><a href="#">Title of first blog post</a></h3>
+                            </div>
+                            <div class="dez-post-meta m-b20">
+                                <ul>
+                                    <li class="post-date"> <i class="fa fa-calendar"></i><strong>10 Aug</strong> <span> 2016</span> </li>
+                                    <li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+                                    <li class="post-comment"><i class="fa fa-comments"></i> <a href="#">0 Comments</a> </li>
+                                </ul>
+                            </div>
+                          
+                            <div class="dez-post-text">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy 
+                                    text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum 
+                                    is simply dummy text of the printing and typesetting  printer a galley Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                    Lorem Ipsum has been the industry's standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+                                    make a type specimen  It has urvived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                                    It was popularised in 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+                                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard text 
+                                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
+                                    It has urvived not only five centuries, but also the leap into electronic typesetting.</p>
+                                <blockquote>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Has been the industry's standard text ever since 
+                                    the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimencenturies.</blockquote>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard text 
+                                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
+                                    It has urvived not only five centuries, but also the leap into electronic typesetting.</p>
+                                <h5>Completely Responsive</h5>
+                                <img class="alignleft" width="300" src="<?php echo get_template_directory_uri() ?>/assets/images/blog/grid/pic4.jpg" alt="" />
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only                                 
+                                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                                    Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+                                    has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+                                    type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                                    It was popularised in the 1960s with the releasefive centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                                    It was popularised in the 1960s with the release</p>
+                                <div class="dez-divider bg-gray-dark"></div>
+                                <img class="alignright" width="300" src="<?php echo get_template_directory_uri() ?>/assets/images/blog/grid/pic1.jpg" alt="" />
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only                                 
+                                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                                    Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+                                    has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+                                    type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                                    It was popularised in the 1960s with the release</p>
+                            </div>
+                            <div class="dez-post-tags clear">
+                                <div class="post-tags"> <a href="#">Child </a> <a href="#">Eduction </a> <a href="#">Money </a> <a href="#">Resturent </a> </div>
+                            </div>
+                        </div>
+                        <!-- blog END -->
                     </div>
-                    <div class="dez-post-meta m-b20">
-                        <ul>
-                            <li class="post-date"> <i class="fa fa-calendar"></i><strong>10 Aug</strong> <span> 2016</span> </li>
-                            <li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
-                            <li class="post-comment"><i class="fa fa-comments"></i> <a href="#">0 Comments</a> </li>
-                        </ul>
+                    <!-- Left part END -->
+                    <!-- Side bar start -->
+                    <div class="col-md-3">
+                        <aside class="side-bar">
+                            <div class="widget recent-posts-entry">
+                                <h4 class="widget-title">Recent Posts</h4>
+                                <div class="widget-post-bx">
+                                    <div class="widget-post clearfix">
+                                        <div class="dez-post-media"> <img src="<?php echo get_template_directory_uri() ?>/assets/images/blog/recent-blog/pic1.jpg" width="200" height="143" alt="" /> </div>
+                                        <div class="dez-post-info">
+                                            <div class="dez-post-header">
+                                                <h6 class="post-title">Title of first blog post entry</h6>
+                                            </div>
+                                            <div class="dez-post-meta">
+                                                <ul>
+                                                    <li class="post-author">By Admin</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-post clearfix">
+                                        <div class="dez-post-media"> <img src="<?php echo get_template_directory_uri() ?>/assets/images/blog/recent-blog/pic2.jpg" width="200" height="160" alt="" /> </div>
+                                        <div class="dez-post-info">
+                                            <div class="dez-post-header">
+                                                <h6 class="post-title">Title of first blog post entry</h6>
+                                            </div>
+                                            <div class="dez-post-meta">
+                                                <ul>
+                                                    <li class="post-author">By Admin</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-post clearfix">
+                                        <div class="dez-post-media"> <img src="<?php echo get_template_directory_uri() ?>/assets/images/blog/recent-blog/pic3.jpg" width="200" height="160" alt="" /> </div>
+                                        <div class="dez-post-info">
+                                            <div class="dez-post-header">
+                                                <h6 class="post-title">Title of first blog post entry</h6>
+                                            </div>
+                                            <div class="dez-post-meta">
+                                                <ul>
+                                                    <li class="post-author">By Admin</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                            <div class="widget widget_gallery">
+                                <h5 class="widget-title">Our services</h5>
+                                <ul>
+                                    <li class="img-effect2"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery/pic1.jpg" alt="" /></a> </li>
+                                    <li class="img-effect2"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery/pic2.jpg" alt="" /></a> </li>
+                                    <li class="img-effect2"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery/pic3.jpg" alt="" /></a> </li>
+                                    <li class="img-effect2"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery/pic4.jpg" alt="" /></a> </li>
+                                    <li class="img-effect2"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery/pic5.jpg" alt="" /></a> </li>
+                                    <li class="img-effect2"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/images/gallery/pic6.jpg" alt="" /></a> </li>
+                                </ul>
+                            </div>
+                            
+                        </aside>
                     </div>
-                    <!--<div class="dez-post-media dez-img-effect zoom-slow"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/images/blog/default/thum1.jpg" alt="" /></a> </div>-->
-                    <div class="dez-post-text">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy 
-                            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum 
-                            is simply dummy text of the printing and typesetting  printer a galley Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
-                            make a type specimen  It has urvived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                            It was popularised in 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
-                            publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard text 
-                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
-                            It has urvived not only five centuries, but also the leap into electronic typesetting.</p>
-                        <blockquote>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Has been the industry's standard text ever since 
-                            the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimencenturies.</blockquote>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard text 
-                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
-                            It has urvived not only five centuries, but also the leap into electronic typesetting.</p>
-                        <h5>Completely Responsive</h5>
-                        <img class="alignleft" width="300" src="<?php echo get_template_directory_uri() ?>/assets/images/blog/grid/pic4.jpg" alt="" />
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only                                 
-                            five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-                            of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-                            Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-                            has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-                            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                            It was popularised in the 1960s with the releasefive centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                            It was popularised in the 1960s with the release</p>
-                        <div class="dez-divider bg-gray-dark"></div>
-                        <img class="alignright" width="300" src="<?php echo get_template_directory_uri() ?>/assets/images/blog/grid/pic1.jpg" alt="" />
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only                                 
-                            five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
-                            of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-                            Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-                            has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-                            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-                            It was popularised in the 1960s with the release</p>
-                    </div>
-                    <div class="dez-post-tags clear">
-                        <div class="post-tags"> <a href="#">Child </a> <a href="#">Eduction </a> <a href="#">Money </a> <a href="#">Resturent </a> </div>
-                    </div>
+                    <!-- Side bar END -->
                 </div>
-                
-                <!-- blog END -->
             </div>
         </div>
     </div>
