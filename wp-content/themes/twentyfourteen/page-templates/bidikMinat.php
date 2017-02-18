2f5763685738995d26cbacc5d8d25e4898c1dd6c
najibalkhala <najibalkhala@gmail.com>
2017-02-18 18:02:13 +0700

    PAGE

---
+++ wp-content/themes/twentyfourteen/page-templates/bidikMinat.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Bidik Minat
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+ get_header();
+?>
+
+<?php 
+get_footer();
---
+++ wp-content/themes/twentyfourteen/page-templates/sekolah.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Sekolah
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+ get_header();
+?>
+
+<?php 
+get_footer();
