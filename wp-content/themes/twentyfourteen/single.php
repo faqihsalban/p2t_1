6fac5423a42cc6f91a2aa38d8f17863570187a03
robseptian <robseptian@gmail.com>
2017-02-18 19:50:18 +0700

    single

--- wp-content/themes/twentyfourteen/single.php
+++ wp-content/themes/twentyfourteen/single.php
@@ -9,32 +9,191 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-					// Previous/next post navigation.
-					twentyfourteen_post_nav();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
+	<div class="page-content">
+        <!-- inner page banner -->
+        <div class="dez-bnr-inr overlay-black-middle" style="background-image:url(<?php echo get_template_directory_uri() ?>/images/background/bg4.jpg);">
+            <div class="container">
+                <div class="dez-bnr-inr-entry">
+                    <h1 class="text-white">Blog single on sidebar</h1>
+                </div>
+            </div>
+        </div>
+        <!-- inner page banner END -->
+        <!-- Breadcrumb row -->
+        <div class="breadcrumb-row">
+            <div class="container">
+                <ul class="list-inline">
+                    <li><a href="#">Home</a></li>
+                    <li>Blog single on sidebar</li>
+                </ul>
+            </div>
+        </div>
+        <!-- Breadcrumb row END -->
+        <div class="content-area">
+            <div class="container">
+                <!-- blog start -->
+                <div class="blog-post blog-single">
+                    <div class="dez-post-title ">
+                        <h3 class="post-title"><a href="#">Title of first blog post</a></h3>
+                    </div>
+                    <div class="dez-post-meta m-b20">
+                        <ul>
+                            <li class="post-date"> <i class="fa fa-calendar"></i><strong>10 Aug</strong> <span> 2016</span> </li>
+                            <li class="post-author"><i class="fa fa-user"></i>By <a href="#">demongo</a> </li>
+                            <li class="post-comment"><i class="fa fa-comments"></i> <a href="#">0 Comments</a> </li>
+                        </ul>
+                    </div>
+                    <div class="dez-post-media dez-img-effect zoom-slow"> <a href="#"><img src="<?php echo get_template_directory_uri() ?>/images/blog/default/thum1.jpg" alt="" /></a> </div>
+                    <div class="dez-post-text">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy 
+                            text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum 
+                            is simply dummy text of the printing and typesetting  printer a galley Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+                            make a type specimen  It has urvived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                            It was popularised in 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+                            publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard text 
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
+                            It has urvived not only five centuries, but also the leap into electronic typesetting.</p>
+                        <blockquote>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Has been the industry's standard text ever since 
+                            the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimencenturies.</blockquote>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard text 
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen  
+                            It has urvived not only five centuries, but also the leap into electronic typesetting.</p>
+                        <h5>Completely Responsive</h5>
+                        <img class="alignleft" width="300" src="<?php echo get_template_directory_uri() ?>/images/blog/grid/pic4.jpg" alt="" />
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only                                 
+                            five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                            of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                            Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+                            has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+                            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                            It was popularised in the 1960s with the releasefive centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                            It was popularised in the 1960s with the release</p>
+                        <div class="dez-divider bg-gray-dark"></div>
+                        <img class="alignright" width="300" src="<?php echo get_template_directory_uri() ?>/images/blog/grid/pic1.jpg" alt="" />
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only                                 
+                            five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                            of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                            Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+                            has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+                            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                            It was popularised in the 1960s with the release</p>
+                    </div>
+                    <div class="dez-post-tags clear">
+                        <div class="post-tags"> <a href="#">Child </a> <a href="#">Eduction </a> <a href="#">Money </a> <a href="#">Resturent </a> </div>
+                    </div>
+                </div>
+                <div class="clear" id="comment-list">
+                    <div class="comments-area" id="comments">
+                        <h2 class="comments-title">8 Comments</h2>
+                        <div class="p-a30 bg-white">
+                            <!-- comment list END -->
+                            <ol class="comment-list">
+                                <li class="comment">
+                                    <div class="comment-body">
+                                        <div class="comment-author vcard"> <img class="avatar photo" src="<?php echo get_template_directory_uri() ?>/images/testimonials/pic1.jpg" alt="" /> <cite class="fn">Stacy poe</cite> <span class="says">says:</span> </div>
+                                        <div class="comment-meta"> <a href="#">October 6, 2015 at 7:15 am</a> </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neqnsectetur adipiscing elit. Nam viae neqnsectetur adipiscing elit.
+                                            Nam vitae neque vitae sapien malesuada aliquet. </p>
+                                        <div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
+                                    </div>
+                                    <ol class="children">
+                                        </ol></li><li class="comment odd parent">
+                                            <div class="comment-body">
+                                                <div class="comment-author vcard"> <img class="avatar photo" src="<?php echo get_template_directory_uri() ?>/images/testimonials/pic2.jpg" alt="" /> <cite class="fn">Stacy poe</cite> <span class="says">says:</span> </div>
+                                                <div class="comment-meta"> <a href="#">October 6, 2015 at 7:15 am</a> </div>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet. 
+                                                    In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis, 
+                                                    ac elementum ligula blandit ac.</p>
+                                                <div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
+                                            </div>
+                                            <ol class="children">
+                                                </ol></li><li class="comment odd parent">
+                                                    <div class="comment-body">
+                                                        <div class="comment-author vcard"> <img class="avatar photo" src="<?php echo get_template_directory_uri() ?>/images/testimonials/pic3.jpg" alt="" /> <cite class="fn">Stacy poe</cite> <span class="says">says:</span> </div>
+                                                        <div class="comment-meta"> <a href="#">October 6, 2015 at 7:15 am</a> </div>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet. 
+                                                            In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis, 
+                                                            ac elementum ligula blandit ac.</p>
+                                                        <div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
+                                                    </div>
+                                                </li>
+                                            
+                                            <!-- list END -->
+                                        
+                                    
+                                    <!-- list END -->
+                                
+                                <li class="comment">
+                                    <div class="comment-body">
+                                        <div class="comment-author vcard"> <img class="avatar photo" src="<?php echo get_template_directory_uri() ?>/images/testimonials/pic1.jpg" alt="" /> <cite class="fn">Stacy poe</cite> <span class="says">says:</span> </div>
+                                        <div class="comment-meta"> <a href="#">October 6, 2015 at 7:15 am</a> </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet. 
+                                            In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis, 
+                                            ac elementum ligula blandit ac.</p>
+                                        <div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
+                                    </div>
+                                </li>
+                                <li class="comment">
+                                    <div class="comment-body">
+                                        <div class="comment-author vcard"> <img class="avatar photo" src="<?php echo get_template_directory_uri() ?>/images/testimonials/pic2.jpg" alt="" /> <cite class="fn">Stacy poe</cite> <span class="says">says:</span> </div>
+                                        <div class="comment-meta"> <a href="#">October 6, 2015 at 7:15 am</a> </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet. 
+                                            In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis, 
+                                            ac elementum ligula blandit ac.</p>
+                                        <div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
+                                    </div>
+                                </li>
+                                <li class="comment">
+                                    <div class="comment-body">
+                                        <div class="comment-author vcard"> <img class="avatar photo" src="<?php echo get_template_directory_uri() ?>/images/testimonials/pic3.jpg" alt="" /> <cite class="fn">Stacy poe</cite> <span class="says">says:</span> </div>
+                                        <div class="comment-meta"> <a href="#">October 6, 2015 at 7:15 am</a> </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet. 
+                                            In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis, 
+                                            ac elementum ligula blandit ac.</p>
+                                        <div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
+                                    </div>
+                                </li>
+                            
+                            <!-- comment list END -->
+                            <!-- Form -->
+                            <div class="comment-respond" id="respond">
+                                <h3 class="comment-reply-title" id="reply-title">Leave a Reply <small> <a style="display:none;" href="#" id="cancel-comment-reply-link" rel="nofollow">Cancel reply</a> </small> </h3>
+                                <form class="comment-form" id="commentform" method="post" action="http://sedatelab.com/developer/donate/wp-comments-post.php" />
+                                    <p class="comment-form-author">
+                                        <label for="author">Name <span class="required">*</span></label>
+                                        <input type="text" value="author" name="author" placeholder="Author" id="author" />
+                                    </p>
+                                    <p class="comment-form-email">
+                                        <label for="email">Email <span class="required">*</span></label>
+                                        <input type="text" value="email" placeholder="Email" name="email" id="email" />
+                                    </p>
+                                    <p class="comment-form-url">
+                                        <label for="url">Website</label>
+                                        <input type="text" value="url" placeholder="Website" name="url" id="url" />
+                                    </p>
+                                    <p class="comment-form-comment">
+                                        <label for="comment">Comment</label>
+                                        <textarea rows="8" name="comment" placeholder="Comment" id="comment"></textarea>
+                                    </p>
+                                    <p class="form-submit">
+                                        <input type="submit" value="Post Comment" class="submit" id="submit" name="submit" />
+                                    </p>
+                                </form>
+                            </div>
+                            <!-- Form END -->
+                        </ol></div>
+                    </div>
+                </div>
+                <!-- blog END -->
+            </div>
+        </div>
+    </div>
 
 <?php
-get_sidebar( 'content' );
-get_sidebar();
+//get_sidebar( 'content' );
+//get_sidebar();
 get_footer();
