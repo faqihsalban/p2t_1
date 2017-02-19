40490c59242e56d75c15ab2ca9892a2745d621cd
najibalkhala <najibalkhala@gmail.com>
2017-02-19 10:56:04 +0700

    aa

--- wp-content/themes/twentyfourteen/page-templates/berita.php
+++ wp-content/themes/twentyfourteen/page-templates/berita.php
@@ -77,10 +77,9 @@ get_header();
                                 </div>
                             </div>
                         <?php endwhile; ?>
-                        <?php if ($the_query->max_num_pages > 1) { // check if the max number of pages is greater than 1  ?>
                         <div class="navigation">
-                            <div class="next-posts"><?php echo get_previous_posts_link('Newer Entries'); // display newer posts link ?></div>
-                            <div class="prev-posts"><?php echo get_next_posts_link('Older Entries', $the_query->max_num_pages); // display older posts link ?></div>
+                            <div class="next-posts"></div>
+                            <div class="prev-posts"></div>
                         </div>
                     </div>
                     <!-- blog grid END -->
@@ -102,13 +101,14 @@ get_header();
                     <!--                    --><?php
                     //
                     //                    get_template_part('loop', 'index'); ?>
+                    <?php if ($the_query->max_num_pages > 1) { // check if the max number of pages is greater than 1  ?>
                     <div class="pagination-bx col-lg-12 clearfix ">
                         <ul class="pagination">
-                            <li class="previous"><a href="#"><i class="fa fa-angle-double-left"></i></a></li>
+                            <li class="previous"><a href="#"><i class="fa fa-angle-double-left"></i><?php echo get_previous_posts_link('Newer Entries'); // display newer posts link ?></a></li>
                             <li class="active"><a href="#">1</a></li>
                             <li><a href="#">2</a></li>
                             <li><a href="#">3</a></li>
-                            <li class="next"><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
+                            <li class="next"><a href="#"><i class="fa fa-angle-double-right"></i><?php echo get_next_posts_link('Older Entries', $the_query->max_num_pages); // display older posts link ?></a></li>
                         </ul>
                     </div>
                     <?php } ?>
