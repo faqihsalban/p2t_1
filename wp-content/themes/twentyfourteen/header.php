8b96d8948dd011971737edd8ad2fcfac80685338
robseptian <robseptian@gmail.com>
2017-02-21 22:11:01 +0700

    asdasdasda

--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -179,7 +179,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<div class="row">
 						<div class="col-md-4">
 							<div class="logo-header "><a href="./index.html"><img src="<?php echo get_template_directory_uri() ?>/assets/images/logo3.png" width="193" height="89" alt="" /></a></div>
-						</div>
+						</div> 
 						
 					</div>
 				</div>
