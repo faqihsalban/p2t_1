56703d8f6183a0e5347f7bc116fbf1edd106417c
najibalkhala <najibalkhala@gmail.com>
2017-02-21 20:32:00 +0700

    jjj

--- wp-content/themes/twentyfourteen/page-templates/berita.php
+++ wp-content/themes/twentyfourteen/page-templates/berita.php
@@ -34,22 +34,22 @@ get_header();
                 <div class="row">
                     <!-- blog grid -->
                     <div id="masonry" class="dez-blog-grid-3">
-                        <!--                        --><?php //$latest = new WP_Query('showposts=8&cat=4'); ?>
-                        <!--                        --><?php //if (have_posts()) : while ($latest->have_posts()) : $latest->the_post(); ?>
-                        <?php
-                        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-                        $query_args = array(
-                            'post_type' => 'post',
-                            'category_name' => 'berita',
-                            'posts_per_page' => 4,
-                            'paged' => $paged
-                        );
-                        // create a new instance of WP_Query
-                        $the_query = new WP_Query($query_args);
-                        ?>
-
-                        <?php if ($the_query->have_posts()) :
-                        while ($the_query->have_posts()) : $the_query->the_post(); // run the loop ?>
+                        <?php $latest = new WP_Query('showposts=8&cat=4'); ?>
+                        <?php if (have_posts()) : while ($latest->have_posts()) : $latest->the_post(); ?>
+                            <?php
+//                        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+//                        $query_args = array(
+//                            'post_type' => 'post',
+//                            'category_name' => 'berita',
+//                            'posts_per_page' => 4,
+//                            'paged' => $paged
+//                        );
+//                        // create a new instance of WP_Query
+//                        $the_query = new WP_Query($query_args);
+//                        ?>
+                            <!---->
+                            <!--                        --><?php //if ($the_query->have_posts()) :
+//                        while ($the_query->have_posts()) : $the_query->the_post(); // run the loop ?>
                             <div class="post card-container col-lg-3 col-md-4 col-sm-6 col-xs-12">
                                 <div class="blog-post blog-grid date-style-2">
                                     <div class="dez-post-media dez-img-effect zoom-slow"><a
@@ -77,10 +77,18 @@ get_header();
                                 </div>
                             </div>
                         <?php endwhile; ?>
-                        <div class="navigation">
-                            <div class="next-posts"></div>
-                            <div class="prev-posts"></div>
-                        </div>
+                            <div class="navigation">
+                                <div class="next-posts"></div>
+                                <div class="prev-posts"></div>
+                            </div>
+                        <?php else : ?>
+
+                            <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                                <h1>Not Found</h1>
+                            </div>
+
+                        <?php endif; ?>
+                        <?php wp_reset_query(); ?>
                     </div>
                     <!-- blog grid END -->
                     <!-- Pagination -->
@@ -101,25 +109,17 @@ get_header();
                     <!--                    --><?php
                     //
                     //                    get_template_part('loop', 'index'); ?>
-                    <?php if ($the_query->max_num_pages > 1) { // check if the max number of pages is greater than 1  ?>
+                    <!--                    --><?php //if ($the_query->max_num_pages > 1) { // check if the max number of pages is greater than 1  ?>
                     <div class="pagination-bx col-lg-12 clearfix ">
                         <ul class="pagination">
-                            <li clas="previous"><?php echo get_next_posts_link( 'Older Entries', $the_query->max_num_pages ); // display older posts link ?></li>
+                            <li clas="previous"><?php echo get_next_posts_link('Older Entries', $the_query->max_num_pages); // display older posts link ?></li>
                             <li class="active"><a href="#">1</a></li>
                             <li><a href="#">2</a></li>
                             <li><a href="#">3</a></li>
                             <li class="next"><?php echo get_previous_posts_link('Newer Entries'); // display newer posts link ?></li>
                         </ul>
                     </div>
-                    <?php } ?>
-                    <?php else : ?>
-
-                        <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                            <h1>Not Found</h1>
-                        </div>
-
-                    <?php endif; ?>
-                    <?php wp_reset_query(); ?>
+                    <!--                    --><?php //} ?>
                     <!-- Pagination END -->
                 </div>
             </div>
