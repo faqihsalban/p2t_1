2eff8687cf3a0ce6322ce379eb06cb05ca6f1743
faqihsalban <faqih.salban@gmail.com>
2017-02-28 21:16:22 +0700

    slider

--- wp-content/themes/twentyfourteen/page-templates/home.php
+++ wp-content/themes/twentyfourteen/page-templates/home.php
@@ -14,49 +14,7 @@ get_header();
        <?php 
     echo do_shortcode("[metaslider id=230]"); 
 ?>
-       
-        <!--Slider Start  -->
- <!--       <div id="jssor_1"
-             style="position:relative;margin-top: 0px;top:0px;left:0px;width:1300px;height:500px;overflow:hidden;visibility:hidden;">
-            <!-- Loading Screen -->
- <!--          <div data-u="loading" style="position:absolute;top:0px;left:0px;background-color:rgba(0,0,0,0.7);">
-                <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-                <div style="position:absolute;display:block;background:url('<?php echo get_template_directory_uri() ?>/assets/images/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
-            </div>
-            <div data-u="slides"
-                 style="cursor:default;position:relative;top:0px;left:0px;width:1300px;height:500px;overflow:hidden;">
-
- //            <?php
-                /* The loop */
-                 
- //                if ( get_post_gallery() ) :
-  //                $gallery = get_post_gallery( get_the_ID(), false );
-                  
-                  /* Loop through all the image and output them one by one */
-  //                foreach( $gallery['src'] AS $src )
-                  {
-                   ?>
-                <div>
-                    <img data-u="image" src="<?php // echo $src;?>"/>
-                </div>
-                <?php 
-            //      }
-                // endif;
-                 
-               ?> 
-            </div>
-            <!-- Bullet Navigator -->
-     <!--        <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
-                <!-- bullet navigator item prototype -->
-<!--                 <div data-u="prototype" style="width:16px;height:16px;"></div>
-            </div>
-            <!-- Arrow Navigator -->
-<!--             <span data-u="arrowleft" class="jssora22l" style="top:0px;left:8px;width:40px;height:58px;"
-                  data-autocenter="2"></span>
-            <span data-u="arrowright" class="jssora22r" style="top:0px;right:8px;width:40px;height:58px;"
-                  data-autocenter="2"></span>
-        </div>
-        <!-- Slider END -->
+      
         
         <!-- About Company -->
         <div class="section-full  bg-white p-t70 p-b40">
