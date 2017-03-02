5c62581aa8f897700c1d373f53f69ddf73cd91a3
najibalkhala <najibalkhala@gmail.com>
2017-03-02 23:14:49 +0700

    aaa

--- wp-content/themes/twentyfourteen/page-templates/home.php
+++ wp-content/themes/twentyfourteen/page-templates/home.php
@@ -144,7 +144,7 @@ get_header();
                                                     alt=""/></a></div>
                                     <div class="dez-info p-a20 border-1 text-center">
                                         <h2 class="dez-title m-t0 m-b10 font-weight-900"><a
-                                                    href="<?php the_permalink(); ?>"><?php echo substr(strip_tags(get_the_title()), 0, 35) . '[...]'; ?></a>
+                                                    href="<?php the_permalink(); ?>"><?php echo substr(strip_tags(get_the_content()), 0, 35) . '[...]'; ?></a>
                                         </h2>
                                         <!--<!--                                        <div class="dez-post-meta m-b20">-->
 
