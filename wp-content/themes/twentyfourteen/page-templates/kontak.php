e4e5319579cd15b5e2caca8c079fc31dd7ac5838
faqih <faqih@MACeotak.local>
2017-03-05 19:56:38 +0700

    aaa

--- wp-content/themes/twentyfourteen/page-templates/kontak.php
+++ wp-content/themes/twentyfourteen/page-templates/kontak.php
@@ -35,9 +35,7 @@
                     <!-- Left part start -->
                     <div class="col-md-9">
                         <h4>Formulir kontak</h4>
-                        <?php 
-                                            echo do_shortcode("[contact-form-7 id=4 title=Contact]");
-                                        ?>
+                        
                         <div id="show_contact_msg"></div>
 
                         <div class="p-a30 bg-white clearfix m-b30">
@@ -46,8 +44,13 @@
                                   <?php the_field('contact_show');?>
                                 </div>
                             </form>
-                                    
+                        <?php 
+                                            echo do_shortcode("[contact-form-7 id=4 title=Contact]");
+                                        ?>            
                         </div>
+
+
+
                         <div class="row">
                             <div class="col-md-12">
                                 <!-- Map part start -->
@@ -56,11 +59,11 @@
                                 <!-- Map part END -->
                             </div>
                         </div>
-<<<<<<< HEAD
+
                         <div class="col-md-12"><iframe width="100%" height="600" src="http://www.mapi.ie/create-google-map/map.php?width=100%&amp;height=600&amp;hl=en&amp;q=universitas%20pendidikan%20indonesia+(Pusat%20Psikologi%20Terapan%20(P2T))&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="http://www.mapsdirections.info/fr/mesurer-distance-surface-google-maps.html">Mesurer distance avec Google Maps</a> en <a href="http://www.mapsdirections.info/fr/">www.mapsdirections.info/fr/</a></iframe></div><br />
-=======
+
                         <div style="col-md-12"><iframe width="100%" height="600" src="http://www.mapi.ie/create-google-map/map.php?width=100%&amp;height=600&amp;hl=en&amp;q=universitas%20pendidikan%20indonesia+(Pusat%20Psikologi%20Terapan%20(P2T))&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="http://www.mapsdirections.info/fr/mesurer-distance-surface-google-maps.html">Mesurer distance avec Google Maps</a> en <a href="http://www.mapsdirections.info/fr/">www.mapsdirections.info/fr/</a></iframe></div><br />
->>>>>>> origin/master
+
                     </div>
                     <!-- Left part END -->
                     <!-- right part start -->
