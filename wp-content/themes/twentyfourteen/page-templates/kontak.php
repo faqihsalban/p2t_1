5be9541db35ee2b26419843be7b6990e9de6c30f
faqih <faqih@172-12-0-35.lightspeed.sgnwmi.sbcglobal.net>
2017-03-06 21:39:25 +0700

    kontak fix

--- wp-content/themes/twentyfourteen/page-templates/kontak.php
+++ wp-content/themes/twentyfourteen/page-templates/kontak.php
@@ -55,7 +55,7 @@
                             <div class="col-md-12">
                                 <!-- Map part start -->
                                 <h4>Lokasi Kami</h4> 
-<!--                                
+
                         <div class="col-md-12"><iframe width="100%" height="600" src="http://www.mapi.ie/create-google-map/map.php?width=100%&amp;height=600&amp;hl=en&amp;q=universitas%20pendidikan%20indonesia+(Pusat%20Psikologi%20Terapan%20(P2T))&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="http://www.mapsdirections.info/fr/mesurer-distance-surface-google-maps.html">Mesurer distance avec Google Maps</a> en <a href="http://www.mapsdirections.info/fr/">www.mapsdirections.info/fr/</a></iframe></div><br />
 
                                 <!-- Map part END -->
